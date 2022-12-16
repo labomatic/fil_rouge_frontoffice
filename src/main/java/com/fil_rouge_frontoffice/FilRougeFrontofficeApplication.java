ab02e5847db714b14b1b44c64b1e6eee3d9f2540
Lisa <lisaabordes@gmail.com>
2022-12-16 16:41:19 +0100

    first commit

---
+++ src/main/java/com/fil_rouge_frontoffice/FilRougeFrontofficeApplication.java
@@ -0,0 +1,13 @@
+package com.fil_rouge_frontoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeFrontofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilRougeFrontofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fil_rouge_frontoffice/FilRougeFrontofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fil_rouge_frontoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeFrontofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
